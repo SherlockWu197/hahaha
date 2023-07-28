444788dddb314b0a76c3aa521acd11b2079152a9
wujiahua <1971170311@qq.com>
2023-07-28 22:43:33 +0800

    完成串口的连接功能

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,8 +7,8 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    InitData();
     InitView();
+    InitData();
     InitConnect();
 }
 
@@ -19,17 +19,94 @@ MainWindow::~MainWindow()
 
 void MainWindow::InitView()
 {
-    QApplication::setApplicationDisplayName("UT343D V1.02");
+    /*初始化按钮状态为失能状态*/
+    ui->btn_OpenSerial->setEnabled(false);
+    ui->btn_CloseSerial->setEnabled(false);
 
+    ui->tabWidget->setTabText(0, QString(tr("Table")));
+    ui->tabWidget->setTabText(1, QString(tr("Histogram")));
+    ui->tabWidget->setTabText(2, QString(tr("Chart")));
 }
 
 void MainWindow::InitData()
 {
+    m_pQSerialPort = new QSerialPort();
 
+    RefreshSerial();
 }
 
 void MainWindow::InitConnect()
 {
+    //connect(  this,&MainWindow::mainwidgetstr,da,&Data::show_data);
+    connect(ui->btn_refreshSerial,&QPushButton::clicked, this, &MainWindow::SlotRefreshSerial);
+    connect(ui->btn_OpenSerial,&QPushButton::clicked, this, &MainWindow::SlotClickConnectSerialBtn);
+    connect(ui->btn_CloseSerial,&QPushButton::clicked, this, &MainWindow::SlotClickCloseSerialBtn);
+}
 
+void MainWindow::RefreshSerial()
+{
+    /*对可用的串口进行查询*/
+    foreach(const QSerialPortInfo &info,QSerialPortInfo::availablePorts())
+    {
+        //测试串口是否可用
+        QSerialPort qserial(info);
+
+        /*如果当前串口为空闲*/
+        if(qserial.open(QIODevice::ReadWrite))
+        {
+            ui->comboBox_Serial->addItem(info.portName());
+            ui->btn_OpenSerial->setEnabled(true);
+            qserial.close();
+        }
+        else
+        {
+            ui->comboBox_Serial->addItem(info.portName()+"(被占用)");
+        }
+    }
+}
+
+void MainWindow::SlotRefreshSerial()
+{
+    ui->comboBox_Serial->clear();
+    InitView();
+    RefreshSerial();
+}
+
+void MainWindow::SlotClickConnectSerialBtn()
+{
+    m_pQSerialPort = new QSerialPort(ui->comboBox_Serial->currentText(),this);//打开串口
+
+    m_pQSerialPort->setBaudRate(QSerialPort::Baud9600); // 设置波特率9600
+    m_pQSerialPort->setDataBits(QSerialPort::Data8); // 设置8数据位
+    m_pQSerialPort->setParity(QSerialPort::NoParity); // 设置无校验位
+    m_pQSerialPort->setStopBits(QSerialPort::OneStop); // 设置1停止位
+    m_pQSerialPort->setFlowControl(QSerialPort::NoFlowControl); // 设置无流控制
+
+    if(!m_pQSerialPort->open(QIODevice::ReadWrite))
+    {
+        qWarning() << "Failed to open serial port:" << m_pQSerialPort->errorString();
+    }
+
+    else
+    {
+        connect(m_pQSerialPort, &QSerialPort::readyRead, [&]() {
+            QByteArray data = m_pQSerialPort->readAll();
+            qDebug() << "Received data:" << data;
+            ui->textBrowser->setText(data);
+        });
+
+        /*连接上串口后更新按钮使能状态*/
+        ui->btn_OpenSerial->setEnabled(false);
+        ui->btn_CloseSerial->setEnabled(true);
+    }
+}
+
+void MainWindow::SlotClickCloseSerialBtn()
+{
+    m_pQSerialPort->clear();
+    m_pQSerialPort->close();
+    m_pQSerialPort->deleteLater();
+    ui->btn_OpenSerial->setEnabled(true);
+    ui->btn_CloseSerial->setEnabled(false);
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -20,11 +20,32 @@ public:
     ~MainWindow();
 
 private:
+    /*初始化界面*/
     void InitView();
+
+    /*初始化数据*/
     void InitData();
+
+    /*初始化信号和槽*/
     void InitConnect();
+
+    /*刷新串口*/
+    void RefreshSerial();
+
+private slots:
+    /*点击刷新串口*/
+    void SlotRefreshSerial();
+
+    /*点击打开串口按钮*/
+    void SlotClickConnectSerialBtn();
+
+    /*点击关闭串口按钮*/
+    void SlotClickCloseSerialBtn();
 
 private:
     Ui::MainWindow *ui;
+    QSerialPort* m_pQSerialPort;
+    QString m_serialportName;   //用于保存串口名
+
 };
 #endif // MAINWINDOW_H
